84523f61b12c930e74d4839893e5c9d61f98ba6b
Nhat Nguyen <natt.lefevre@gmail.com>
2024-07-12 22:15:44 -0700

    cleanup

--- app/mdx-to-components.tsx
+++ app/mdx-to-components.tsx
@@ -1,8 +1,18 @@
+import { ReactNode } from "react"
 import { Fragment, jsx, jsxs } from "react/jsx-runtime"
 import rehypeReact from "rehype-react"
 import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
 import { unified } from "unified"
+
+const components = {
+  h1: ({ children }: { children: ReactNode }) => (
+    <h1 className="mb-4 text-3xl font-bold">{children}</h1>
+  ),
+  p: ({ children }: { children: ReactNode }) => (
+    <p className="mb-2">{children}</p>
+  ),
+}
 
 export async function mdxToComponents(mdx: string) {
   // Order of plugins matters
@@ -12,14 +22,7 @@ export async function mdxToComponents(mdx: string) {
     // @ts-expect-error: suggested in documentation due to missing types
     .use(rehypeReact, {
       Fragment,
-      components: {
-        // Example
-        h1: ({ children }) => (
-          <h1 className="mb-4 text-3xl font-bold">{children}</h1>
-        ),
-        p: ({ children }) => <p className="mb-2">{children}</p>,
-        // a: Link,
-      },
+      components,
       jsx,
       jsxs,
     })
