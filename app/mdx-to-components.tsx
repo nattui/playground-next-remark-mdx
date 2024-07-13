9e34bb0353a17784747483e52039f1d5c36b7a54
Nhat Nguyen <natt.lefevre@gmail.com>
2024-07-12 22:08:59 -0700

    prettier

--- app/mdx-to-components.tsx
+++ app/mdx-to-components.tsx
@@ -1,8 +1,8 @@
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeReact from "rehype-react";
-import { Fragment, jsx, jsxs } from "react/jsx-runtime";
+import { Fragment, jsx, jsxs } from "react/jsx-runtime"
+import rehypeReact from "rehype-react"
+import remarkParse from "remark-parse"
+import remarkRehype from "remark-rehype"
+import { unified } from "unified"
 
 export async function mdxToComponents(mdx: string) {
   // Order of plugins matters
@@ -12,18 +12,18 @@ export async function mdxToComponents(mdx: string) {
     // @ts-expect-error: suggested in documentation due to missing types
     .use(rehypeReact, {
       Fragment,
-      jsx,
-      jsxs,
       components: {
         // Example
         h1: ({ children }) => (
-          <h1 className="text-3xl font-bold mb-4">{children}</h1>
+          <h1 className="mb-4 text-3xl font-bold">{children}</h1>
         ),
-        p: ({ children }) => <p className="text-gray-700 mb-2">{children}</p>,
+        p: ({ children }) => <p className="mb-2">{children}</p>,
         // a: Link,
       },
+      jsx,
+      jsxs,
     })
-    .process(mdx);
+    .process(mdx)
 
-  return result;
+  return result
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import { mdxToComponents } from "@/app/mdx-to-components";
+import { mdxToComponents } from "@/app/mdx-to-components"
 
 export default async function Home() {
   const mdx = `
   # Hello
 
   my name is Natt
-  `;
+  `
 
-  const components = mdxToComponents(mdx);
+  const components = mdxToComponents(mdx)
 
   return (
     <main className="p-16">
       <p className="mb-4">My custom component</p>
       {components}
     </main>
-  );
+  )
 }
